ca5a90263450a8d36e1f6bf3fe3a451735d348c6
Ido Mandelman <idomand@gmail.com>
2023-10-01 16:26:33 +0200

    adding first component

--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -1,12 +1,22 @@
 <template>
   <header class="w-full">
-    <h2 class="bg-brand-green-1 text-brand-grey-1 pl-5">Main Nav</h2>
+    <div class="fixed left-0 top-0 h-16 w-full bg-white">
+      <div class="border-brand-grey-1 mx-auto flex h-full flex-nowrap border-b border-solid px-8">
+        <a class="flex h-full items-center text-xl" href="/">{{ company }} </a>
+        <h2 class="ml-8 flex h-full items-center">
+          Develop by {{ author.firstName }} {{ author.lastName }}
+        </h2>
+      </div>
+    </div>
   </header>
 </template>
 
 <script>
 export default {
-  name: 'MainNav'
+  name: 'MainNav',
+  data() {
+    return { company: 'Bobo careers', author: { firstName: 'ido', lastName: 'Mandelman' } }
+  }
 }
 </script>
 
