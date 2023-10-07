236986428fd85cce9cc5002af9af05e9baf21ef8
Ido Mandelman <idomand@gmail.com>
2023-10-07 16:17:01 +0200

    adding sign in option

---
+++ src/components/ActionButton.vue
@@ -0,0 +1,17 @@
+<template>
+  <button
+    class="rounded border-0 px-5 py-3 font-medium text-white bg-brand-blue-1 hover:shadow-blue transition"
+    @click="handleClick"
+  >
+    Sign In
+  </button>
+</template>
+<script>
+export default {
+  name: "ActionButton",
+  components: {},
+  methods: {
+    handleClick() {},
+  },
+};
+</script>
---
+++ src/components/ProfileImage.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="h-full">
+    <img
+      ref="myElement"
+      class="h-14 object-contain rounded"
+      :src="imgSrc"
+      alt="user profile image"
+    />
+  </div>
+</template>
+<script>
+export default {
+  name: "ProfileImage",
+  data() {
+    return {
+      imgSrc:
+        "https://images.dog.ceo/breeds/spaniel-blenheim/n02086646_3832.jpg",
+    };
+  },
+};
+</script>
