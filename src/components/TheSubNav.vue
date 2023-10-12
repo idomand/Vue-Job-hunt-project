e5a2d59719f322cd3a32109b4da68f30f2f13d30
Ido Mandelman <idomand@gmail.com>
2023-10-12 21:32:32 +0200

    Create TheSubNav.vue

---
+++ src/components/TheSubNav.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="bg-white w-full h-16 border-b border-solid border-brand-gray-1">
+    <div v-if="onJobResultsPage" class="flex items-center h-full px-8">
+      <div>
+        <font-awesome-icon :icon="['fas', 'search']" class="mr-3" />
+      </div>
+      <div><span class="text-brand-green-1">1653</span> jobs matched</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TheSubNav",
+  data() {
+    return { onJobResultsPage: true };
+  },
+};
+</script>
