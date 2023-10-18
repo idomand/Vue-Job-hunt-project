e3b7bf439f6f8fe75f241ff27226f66ff7c00a97
Ido Mandelman <idomand@gmail.com>
2023-10-18 13:55:14 +0200

    adding Job results elements

--- src/Views/JobResultsView.vue
+++ src/Views/JobResultsView.vue
@@ -1,28 +1,19 @@
 <template>
-  <h1 class="text-6xl font-bold">JobResultsView</h1>
-  <h1 class="text-6xl font-bold">I'm looking for {{ getRole }}</h1>
-  <h1 class="text-6xl font-bold">in {{ getLocation }}</h1>
-
-  <button @click="goHome">got to home</button>
+  <div class="flex w-full flex-row flex-nowrap">
+    <job-filters-sidebar />
+    <job-listings />
+  </div>
 </template>
 
 <script>
+import JobFiltersSidebar from "@/components/JobResults/JobFiltersSidebar/JobFiltersSidebar.vue";
+import JobListings from "@/components/JobResults/JobListings.vue";
+
 export default {
   name: "JobResultsView",
-  components: {},
-  computed: {
-    getRole() {
-      console.log("this.$route.params.role :>> ", this.$route);
-      return this.$route.query.role;
-    },
-    getLocation() {
-      return this.$route.query.location;
-    },
-  },
-  methods: {
-    goHome() {
-      this.$router.push("/");
-    },
+  components: {
+    JobFiltersSidebar,
+    JobListings,
   },
 };
 </script>
